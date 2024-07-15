cb7ada19912710069ea64ab1e59fe5b9574edae9
thiago_dias1985 <diasthiagojose@gmail.com>
2024-07-15 19:05:11 -0300

    first-exercises

---
+++ inicio1.rb/estudo_for.rb
@@ -0,0 +1,8 @@
+frutas = ['maçã','uva','banana','pêra']
+count = 1                              # count é um contador
+for fruta in frutas                    # for é usado para percorrer uma coleção de elementos
+  puts fruta                           #puts imprime semelhante ao print
+  puts count
+  count = count + 1                    # count + 1 fez com que o contador desse um número para cada produto da lista
+
+end                                    #end marca o final da interação
---
+++ inicio1.rb/estudo_while.rb
@@ -0,0 +1,5 @@
+numero = 1
+while numero < 11
+  puts numero
+  numero += 1
+end
